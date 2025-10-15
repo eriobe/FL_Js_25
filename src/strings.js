90341d4e920da0848fbb92fe450fda071d30134a
Erik Öberg <erik.oberg@miun.se>
2025-10-15 10:51:46 +0200

    grunder

---
+++ src/strings.js
@@ -0,0 +1,26 @@
+let first = "hej";
+// backticks
+// enligt SAOL bakåtfnuttar
+let second = `hopp`;
+
+console.log(second);
+const a = 1;
+const b = 3;
+let sammanslagen = `${a} + ${b} = ${a + b}`;
+
+console.log(sammanslagen);
+
+console.log("1: ", false == 0);
+console.log("1: ", false === 0);
+
+/*
+Bygg en toNamn-funktion
+Skriv en funktion toNamn(förnamn, efternamn) som returnerar "Efternamn, Förnamn".
+
+RETURNERA
+
+stretch
+Går det att få funktionen att hantera om jag inte skickar både för och efternamn??
+*/
+toNamn(Erik, Öberg, piggelin);
+toNamn(Erik);
